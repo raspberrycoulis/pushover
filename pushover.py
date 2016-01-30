9fa3ea37760ec3d1ce0a44ffabb08ff75b646260
Wesley Archer <raspberrycoulis@users.noreply.github.com>
2016-01-30 12:06:44 +0000

    Update pushover.py

--- pushover.py
+++ pushover.py
@@ -9,7 +9,7 @@ conn.request("POST", "/1/messages.json",
     "user": "USER_TOKEN",                       # Insert user token here
     "html": "1",                                # 1 for HTML, 0 to disable
     "title": "Motion Detected!",                # Title of the message
-    "message": "<b>Front Door</b> camera!",     # Content of the message (can use HTML)
+    "message": "<b>Front Door</b> camera!",     # Content of the message
     "url": "http://IP.ADD.RE.SS",               # Link to be included in message
     "url_title": "View live stream",            # Text for the link
     "sound": "siren",                           # Define the sound played on the receiving device
